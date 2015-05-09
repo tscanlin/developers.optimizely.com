6a52c7e16b05b395afe629f024c79bd856417174
Tim Scanlin <tscanlin@gmail.com>
2015-05-08 18:18:48 -0700

    work on toc stuff

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,25 @@
 // Initialize code syntax highlighting.
 hljs.initHighlightingOnLoad();
 
+var smoothScroll = require('smooth-scroll');
+smoothScroll.init({
+  easing: 'easeInOutCubic',
+  offset: 20,
+  speed: 1000,
+  updateURL: true,
+  callbackBefore: function(toggle, anchor) {
+    console.log(toggle, anchor);
+    debugger;
+  },
+  callbackAfter: function(toggle, anchor) {
+    console.log(toggle, anchor);
+    debugger;
+  }
+});
+
+// window.root = window;
+// smoothScroll.animateScroll( null, '#change-log' );
+
 var each = [].forEach;
 
 // Document elements.
@@ -9,43 +28,62 @@ var header = document.getElementById('header');
 var nav = document.getElementById('nav');
 var content = document.getElementById('content');
 
-var quickjump = document.querySelector('.quickjump');
+var toc = document.querySelector('.active + .toc');
 var headings = document.querySelectorAll('h2, h3');
 var collapsers = document.querySelectorAll('[data-collapser]');
 
 var IS_COLLAPSED_CLASS = 'is-collapsed';
 
-var topPath = '/' + window.location.pathname.split('/')[1] + '/';
+// var topPath = '/' + window.location.pathname.split('/')[1] + '/';
 
 
-// QUICKJUMP STUFF
+// TOC STUFF
 var subGroup, lastHeading;
 // Iterate over headings.
+each.call(headings, function(heading, i) {
+  // If the page has the toc container.
+  if (toc && headings.length > 0) {
+    if (heading.nodeName === 'H2') {
+      toc.appendChild(makeLink(heading));
+      lastHeading = heading;
+    }
+    else if (heading.nodeName === 'H3') {
+      if (lastHeading.nodeName === 'H2') {
+        subGroup = document.createElement('ul');
+        subGroup.setAttribute('class', 'unstyled soft--left');
+        toc.appendChild(subGroup);
+      }
+      subGroup.appendChild(makeLink(heading));
+      lastHeading = heading;
+    }
+  }
 
-// each.call(headings, function(heading) {
-//   // If the page has the quickjump container.
-//   if (quickjump && headings.length > 0) {
-//     if (heading.nodeName === 'H2') {
-//       quickjump.appendChild(makeLink(heading));
-//       lastHeading = heading;
-//     }
-//     else if (heading.nodeName === 'H3') {
-//       if (lastHeading.nodeName === 'H2') {
-//         subGroup = document.createElement('ul');
-//         subGroup.setAttribute('class', 'soft--left');
-//         quickjump.appendChild(subGroup);
-//       }
-//       subGroup.appendChild(makeLink(heading));
-//       lastHeading = heading;
-//     }
-//   }
-// });
+  if (toc && i === (headings.length - 1)) {
+    // console.log(i, smoothScroll);
+    // smoothScroll.init({
+    //   easing: 'easeInOutCubic',
+    //   offset: 20,
+    //   speed: 1000,
+    //   updateURL: true,
+    //   callbackBefore: function(toggle, anchor) {
+    //     console.log(toggle, anchor);
+    //     debugger;
+    //   },
+    //   callbackAfter: function(toggle, anchor) {
+    //     console.log(toggle, anchor);
+    //     debugger;
+    //   }
+    // });
+  }
+});
+
 
 // Make list item + link.
 function makeLink(heading) {
   var item = document.createElement('li');
   var a = document.createElement('a');
   a.textContent = heading.textContent;
+  a.setAttribute('data-scroll', '');
   a.setAttribute('href', '#' + heading.id);
   a.setAttribute('class', 'quickjump__link soft--left');
   item.appendChild(a);
@@ -60,11 +98,13 @@ function updateSidebar() {
   var top = document.documentElement.scrollTop || body.scrollTop;
   var headerHeight = header.offsetHeight;
 
-  if (top > headerHeight) {
-    nav.className = 'fixed';
-  }
-  else {
-    nav.className = '';
+  if (nav) {
+    if (top > headerHeight) {
+      nav.className = 'fixed';
+    }
+    else {
+      nav.className = '';
+    }
   }
 }
 
@@ -72,10 +112,6 @@ function updateSidebar() {
 
 // Make pseudo directive for collapsible sections.
 each.call(collapsers, function(collapser) {
-  // if (collapser.getAttribute('href') === topPath) {
-  //   collapser.nextElementSibling.classList.remove(IS_COLLAPSED_CLASS);
-  // }
-
   collapser.addEventListener('click', function(e) {
     e.preventDefault();
 
