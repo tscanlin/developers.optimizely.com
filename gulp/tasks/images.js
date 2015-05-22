e060f949990b7f13704f2e165ecd2f491f6d0fef
Tim Scanlin <tscanlin@gmail.com>
2015-05-22 13:35:53 -0700

    add image support for .gif

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -8,6 +8,7 @@ var paths        = require('../../config').paths;
 
 gulp.task('images', function() {
   return gulp.src([
+      path.join(paths.src + paths.img, '**/*.gif'),
       path.join(paths.src + paths.img, '**/*.svg'),
       path.join(paths.src + paths.img, '**/*.png')
     ])
