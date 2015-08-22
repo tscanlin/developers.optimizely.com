3f7b14db1436d77ecba4e0a6bfdccc159b349015
Tim Scanlin <tscanlin@users.noreply.github.com>
2015-08-22 01:06:21 -0700

    Update default.js

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -2,12 +2,7 @@ var gulp = require('gulp');
 
 gulp.task('default', [
   // 'markdown',
-  'concat',
-  'html-templates',
-  'copy-files',
-  'sass',
-  'browserify',
-  'images',
+  'build',
   'browser-sync',
   'watch',
 ]);
