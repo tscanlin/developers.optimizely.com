6f027aaa1c7bf964893b9d32e4f45e3c05bc64a8
Tim Scanlin <tscanlin@gmail.com>
2016-03-29 17:34:25 -0700

    deploy with travis

---
+++ travis-deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+set -o errexit
+
+# exit if its not the master branch
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master. No deploy."
+  exit 0
+fi
+
+
+# config
+git config --global user.email "nobody@nobody.org"
+git config --global user.name "Travis CI"
+
+# build steps (taken care of beforehand)
+
+# deploy
+cd build
+git init
+git add .
+rev=$(git rev-parse --short HEAD)
+git commit -m "Deploy (${rev}) to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
