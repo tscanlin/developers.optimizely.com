65cb1ea78f6c1c2604cf66a2bfcea29305bf9844
Tim Scanlin <tscanlin@gmail.com>
2016-03-30 17:20:44 -0700

    add travis commit message to gh-pages commit

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -32,5 +32,5 @@ git config --global user.name "Travis CI"
 cd build
 git init
 git add .
-git commit -m "Deploy to Github Pages"
+git commit -m "Deploy: $TRAVIS_COMMIT_MESSAGE"
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
